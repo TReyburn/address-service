6521fa59da0171939075ef7c23fdfc0b3ae20304
Travis Reyburn <travisreyburn@gmail.com>
2020-11-30 20:51:32 -0700

    added basic main stub

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	parser "github.com/openvenues/gopostal/parser"
+)
+
 func main() {
-	
+	parsed := parser.ParseAddress("781 Franklin Ave Crown Heights Brooklyn NY 11216 USA")
+	fmt.Println(parsed)
 }
