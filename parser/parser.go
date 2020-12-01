1f4c231e289aafe0b1e61cfda8185b3df22e5215
Travis Reyburn <treyburn@alteryx.com>
2020-12-01 11:13:15 -0700

    added convert address func and now returning address struct via chan

--- parser/parser.go
+++ parser/parser.go
@@ -1,10 +1,13 @@
 package parser
 
 import (
+	"fmt"
+
 	parser "github.com/openvenues/gopostal/parser"
 )
 
-type address struct {
+// Address struct containing all possible address fields
+type Address struct {
 	house         string
 	category      string
 	near          string
@@ -28,8 +31,58 @@ type address struct {
 }
 
 // ParseAddress takes an address and a channel to coommunicate on
-func ParseAddress(addr string, c chan []parser.ParsedComponent) {
+func ParseAddress(addr string, c chan Address) {
 	parsed := parser.ParseAddress(addr)
-	// fmt.Println(parsed)
-	c <- parsed
+	go convertAddress(parsed, c)
+}
+
+func convertAddress(pc []parser.ParsedComponent, c chan Address) {
+	addr := Address{}
+	for _, c := range pc {
+		switch c.Label {
+		case "house":
+			addr.house = c.Value
+		case "category":
+			addr.category = c.Value
+		case "near":
+			addr.near = c.Value
+		case "house_number":
+			addr.houseNumber = c.Value
+		case "road":
+			addr.road = c.Value
+		case "unit":
+			addr.unit = c.Value
+		case "level":
+			addr.level = c.Value
+		case "staircase":
+			addr.staircase = c.Value
+		case "entrance":
+			addr.entrance = c.Value
+		case "po_box":
+			addr.poBox = c.Value
+		case "postcode":
+			addr.postcode = c.Value
+		case "suburb":
+			addr.suburb = c.Value
+		case "city_district":
+			addr.cityDistrict = c.Value
+		case "city":
+			addr.city = c.Value
+		case "island":
+			addr.island = c.Value
+		case "state_district":
+			addr.stateDistrict = c.Value
+		case "state":
+			addr.state = c.Value
+		case "country_region":
+			addr.countryRegion = c.Value
+		case "country":
+			addr.country = c.Value
+		case "world_region":
+			addr.worldRegion = c.Value
+		default:
+			fmt.Println("Did not recognize", c.Label)
+		}
+	}
+	c <- addr
 }
