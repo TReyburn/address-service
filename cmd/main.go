96b3d4d9cdb4e4f8c6b36d57b5d7c3856b15232d
Travis Reyburn <travisreyburn@gmail.com>
2020-11-30 21:17:53 -0700

    fixed path

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"address-service/addr-parser"
+	"github.com/TReyburn/address-service/addr-parser"
 )
 
 func main() {
 	someStr := "20544 CR 138 Goshen IN 46526"
-	res := pA(someStr)
+	res := addr_parser.pA(someStr)
 	fmt.Println(res)
 }
