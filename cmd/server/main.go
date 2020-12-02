de276e085df1cf12dd93e27875b5a48d3814a219
Travis Reyburn <treyburn@alteryx.com>
2020-12-02 16:22:23 -0700

    making progress implementing server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
 	"github.com/TReyburn/address-service/addresspb"
+	"github.com/TReyburn/address-service/parser"
 	"google.golang.org/grpc"
 )
 
 type server struct{}
+
+func (s server) AddressParse(_ context.Context, req *addresspb.APRequest) (*addresspb.APResponse, error) {
+	a := req.GetAddress()
+	c := make(chan addresspb.APResponse)
+	go parser.ParseAddress(a, c)
+
+}
 
 func main() {
 	log.Println("Server starting up")
@@ -19,5 +28,5 @@ func main() {
 	}
 
 	s := grpc.NewServer()
-	addresspb.RegisterAddressParseService()
+	addresspb.RegisterAddressParseServiceServer(s, server{})
 }
