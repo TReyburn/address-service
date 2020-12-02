4ef87d17d2f515c2efacbec960e98f31276ab2e3
Travis Reyburn <treyburn@alteryx.com>
2020-12-02 16:30:33 -0700

    finished implementing our server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -14,19 +14,24 @@ type server struct{}
 
 func (s server) AddressParse(_ context.Context, req *addresspb.APRequest) (*addresspb.APResponse, error) {
 	a := req.GetAddress()
-	c := make(chan addresspb.APResponse)
+	c := make(chan *addresspb.APResponse)
 	go parser.ParseAddress(a, c)
-
+	return <-c, nil
 }
 
 func main() {
 	log.Println("Server starting up")
+	parser.InitParser()
 
-	lis, err := net.Listen("tcp", "0.0.0.0.50051")
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
 		log.Fatalln("Failed to listen:", err)
 	}
 
 	s := grpc.NewServer()
 	addresspb.RegisterAddressParseServiceServer(s, server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalln("Failed to serve:", err)
+	}
 }
